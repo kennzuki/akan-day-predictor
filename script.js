f437e1848904f0d8a3feead5ffc51c20be926336
kennzuki <kingsyken@gmail.com>
2024-04-15 20:54:15 +0300

    adding an event listner for the form and prevent default behavior

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //getting the variables form
 let dataForm = document.getElementById('dataform');
 let gender = document.getElementById("gender")
-let result=document.getElementById("result")
+let result = document.getElementById("result")
+
+//getting the form data
+
+dataForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
